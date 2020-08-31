87cc0f7c458d23485f2c83fc8b6fe1ebe732a932
takerumimata <fossil1908@gmail.com>
2020-08-31 17:42:53 +0900

    mutexを利用した排他制御

---
+++ mutex.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	var count int
+	var lock sync.Mutex
+
+	increment := func() {
+		lock.Lock()
+		defer lock.Unlock()
+		count++
+		fmt.Printf("incrementing: %d\n", count)
+	}
+
+	decrement := func() {
+		lock.Lock()
+		defer lock.Unlock()
+		count--
+		fmt.Printf("decrementing: %d\n", count)
+	}
+
+	// インクリメント
+	var arithmetic sync.WaitGroup
+	for i := 0; i <= 5; i++ {
+		arithmetic.Add(1)
+		go func() {
+			defer arithmetic.Done()
+			increment()
+		}()
+	}
+
+	// デクリメント
+	for i := 0; i <= 5; i++ {
+		arithmetic.Add(1)
+		go func() {
+			defer arithmetic.Done()
+			decrement()
+		}()
+	}
+
+	arithmetic.Wait()
+
+	fmt.Println("Arithmetic complete.")
+}
+
+/*
+一つあたりのgoruitinがどのくらいメモリ食ってるかを示す
+*/
